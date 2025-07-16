95302e53000c4c2c291edb40af080af462e456e7
tinuademargaret <tinuademargaret@gmail.com>
2025-07-16 15:04:55 +0100

    disable sleep mode

--- tinyzerocodeq/ppo_trainer/run_qwen2.5.sh
+++ tinyzerocodeq/ppo_trainer/run_qwen2.5.sh
@@ -40,7 +40,6 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
-    actor_rollout_ref.ref.fsdp_config.param_offload=True \
     critic.optim.lr=1e-5 \
     critic.model.use_remove_padding=True \
     critic.model.path=Qwen/Qwen2-0.5B-Instruct \
